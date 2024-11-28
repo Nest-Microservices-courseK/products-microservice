cd92f424ea5a564208e7a167564c164abf7a599c
kevinefraim <kevinefraim02@gmail.com>
2024-11-28 19:25:24 -0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
     { transport: Transport.TCP, options: { port: envs.PORT } },
   );
 
-  const logger = new Logger('Main');
+  const logger = new Logger('Products-Main');
 
 
   app.useGlobalPipes(
